9c8ca1450f9980820b2ea694c43cb3f6114b94d8
r-kubot <r-kubota@i-enter.co.jp>
2023-05-25 10:05:07 +0900

    Chapter8まで完了

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 久保田 梨央 on 2023/05/24.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images" //Storage内の画像ファイルの保存場所
+    static let PostPath = "posts" //Firestore内の投稿データの保存場所
+}
